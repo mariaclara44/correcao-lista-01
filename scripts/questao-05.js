d6c34a77d4a9abaa2c1c701fe60a8ae9f5031252
Maria Clara <maria.c.ferreira47@aluno.senai.br>
2025-05-19 11:39:48 -0300

    feat: finaliza resolução da questão 05

---
+++ scripts/questao-05.js
@@ -0,0 +1,34 @@
+const precos = [150, 50, 220, 80, 120, 30, 480, 70, 90, 190];
+const precosComDesconto = [];
+let contadorArrayPrecoDesconto = 0;
+let preco = 0;
+let desconto = 0;
+let somaTotalPrecos = 0;
+let somaTotalPrecosComDesconto = 0;
+let perda = 0;
+let mediaDesconto = 0;
+
+
+//Varredura de array com laço de repetição
+for (let i = 0; i < precos.length; i++) {
+    preco = precos[i];
+    if (precos[i] > 100) {
+        desconto = precos[i] * 0.1 //10/100
+        preco = precos[i] - desconto;
+    }
+    
+precosComDesconto[contadorArrayPrecoDesconto] = preco
+contadorArrayPrecoDesconto++;
+
+somaTotalPrecos += precos[i];
+somaTotalPrecosComDesconto += precosComDesconto[i];
+}
+
+perda = somaTotalPrecos - somaTotalPrecosComDesconto;
+mediaDesconto = somaTotalPrecosComDesconto / precosComDesconto.length;
+
+console.table(precos);
+console.table(precosComDesconto);
+console.log(`A loja vai perder: R$ ${perda.toFixed(2)}`);
+console.log(`A média dos valores com descontos é: R$ ${mediaDesconto.toFixed(2)}`);
+
